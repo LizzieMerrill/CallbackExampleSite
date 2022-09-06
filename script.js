dd391c79ead9edf9b26ef3a589de8ce6ac479f6b
LizzieMerrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-09-05 17:00:18 -0700

    Create script.js
    Initial work

---
+++ script.js
@@ -0,0 +1,13 @@
+function submit(){
+    let weekStatus = document.getElementById('#variable-status').value;
+
+    if (weekStatus == 1){
+        alert("That\'s good! :)");
+        1 = good
+    }
+    else if(weekStatus == 2){
+        alert("That\'s bad! :(");
+        //2 = bad
+    }
+    else{}
+}
